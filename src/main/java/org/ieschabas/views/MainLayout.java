f0d68d1a4207b9fe6c4f4735c7f215759b0b7fcc
start.vaadin.com <no-reply@vaadin.com>
2023-02-16 19:44:23 +0000

    Generated project

---
+++ src/main/java/org/ieschabas/views/MainLayout.java
@@ -0,0 +1,82 @@
+package org.ieschabas.views;
+
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.ieschabas.components.appnav.AppNav;
+import org.ieschabas.components.appnav.AppNavItem;
+import org.ieschabas.views.actores.ActoresView;
+import org.ieschabas.views.directores.DirectoresView;
+import org.ieschabas.views.empty3.Empty3View;
+import org.ieschabas.views.películas.PelículasView;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H2 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
+    }
+
+    private void addDrawerContent() {
+        H1 appName = new H1("AE3-PRO-FRONTEND");
+        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        Header header = new Header(appName);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(header, scroller, createFooter());
+    }
+
+    private AppNav createNavigation() {
+        // AppNav is not yet an official component.
+        // For documentation, visit https://github.com/vaadin/vcf-nav#readme
+        AppNav nav = new AppNav();
+
+        nav.addItem(new AppNavItem("Películas", PelículasView.class, "la la-film"));
+        nav.addItem(new AppNavItem("Actores", ActoresView.class, "la la-user"));
+        nav.addItem(new AppNavItem("Directores", DirectoresView.class, "la la-user-tie"));
+        nav.addItem(new AppNavItem("Empty3", Empty3View.class, "la la-file"));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
---
+++ src/main/java/org/ieschabas/views/empty3/Empty3View.java
@@ -0,0 +1,34 @@
+package org.ieschabas.views.empty3;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import org.ieschabas.views.MainLayout;
+
+@PageTitle("Empty3")
+@Route(value = "empty3", layout = MainLayout.class)
+public class Empty3View extends VerticalLayout {
+
+    public Empty3View() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
