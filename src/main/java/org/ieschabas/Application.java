07025d59149ca1eafa1bc48ed8278b3b9ba3f9cf
Antonio Mas <antoniomas33@hotmail.com>
2023-05-02 16:10:21 +0200

    cambios de pwa 1

--- src/main/java/org/ieschabas/Application.java
+++ src/main/java/org/ieschabas/Application.java
@@ -2,6 +2,7 @@ package org.ieschabas;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Theme(value = "AE3-PRO-FRONTEND", variant = Lumo.DARK)
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@PWA(name="videoclub pro app", shortName = "Videoclub Pro", iconPath = "icons/PRO_LOGO.png")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
