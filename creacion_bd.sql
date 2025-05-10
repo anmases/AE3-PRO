bb3d51c9b6cd130f30b94103991369aaf69349b0
Antonio Mas <antoniomas33@hotmail.com>
2025-05-10 22:18:47 +0200

    .

--- creacion_bd.sql
+++ creacion_bd.sql
@@ -28,9 +28,9 @@ nombre VARCHAR(50) NOT NULL,
 apellidos VARCHAR(50) NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE,
 contrasenya TEXT NOT NULL,
-direccion VARCHAR(100),
-activo BOOLEAN NOT NULL DEFAULT TRUE,
-fecha_registro DATE NOT NULL DEFAULT CURRENT_DATE,
+direccion VARCHAR(100) NOT NULL,
+activo BOOLEAN NOT NULL,
+fecha_registro DATE NOT NULL,
 rol ENUM ("ADMIN","USER")
 );
 CREATE TABLE alquiler(
@@ -50,8 +50,8 @@ completado BOOLEAN NOT NULL
 );
 
 /*Valores de muestra*/
-INSERT INTO usuario (nombre, apellidos, email, contrasenya, rol)
-VALUES ("admin", "admin", "admin@admin.com", "$2y$10$/zY780DscVJryXd7Ij5VFu/rrfaDgsl7B32pMBVfWF9Z.AgfW5eHG", "ADMIN");
+INSERT INTO usuario (nombre, apellidos, email, direccion, activo, contrasenya, fecha_registro, rol)
+VALUES ("admin", "admin", "admin@admin.com", "", 1, "$2y$10$/zY780DscVJryXd7Ij5VFu/rrfaDgsl7B32pMBVfWF9Z.AgfW5eHG", CURDATE(),"ADMIN");
 
 INSERT INTO pelicula (titulo, descripcion, anyo_publicacion, duracion, categoria, formato, valoracion, caratula, url) 
 VALUES ("El Padrino","América, años 40. Don Vito Corleone (Marlon Brando) es el respetado y temido jefe de una de las cinco familias de la mafia de Nueva York. Tiene cuatro hijos: Connie (Talia Shire), el impulsivo Sonny (James Caan), el pusilánime Fredo (John Cazale) y Michael (Al Pacino), que no quiere saber nada de los negocios de su padre. Cuando Corleone, en contra de los consejos de Il consigliere Tom Hagen (Robert Duvall), se niega a participar en el negocio de las drogas, el jefe de otra banda ordena su asesinato. Empieza entonces una violenta y cruenta guerra entre las familias mafiosas",1972,175,'DRAMA','DIVX','CINCO',
